d816310cdebea123e6b8afbd266913bf540ab432
unknown <ndoja.damian@gmail.com>
2018-11-17 18:30:52 +0100

    Updates

---
+++ precache-manifest.fe2a09ac43196377227537924985b547.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "87e0fcc032590a320c36c5caaf642e72",
+    "url": "/Transcend/static/media/transcend_title.87e0fcc0.svg"
+  },
+  {
+    "revision": "f1cab7eabde5e78612e7ebc67a80d247",
+    "url": "/Transcend/static/media/transcend_logo_colored.f1cab7ea.svg"
+  },
+  {
+    "revision": "6a51bad80108ade2d566dfc9e99ebaf1",
+    "url": "/Transcend/static/media/transcend_down_arrow.6a51bad8.svg"
+  },
+  {
+    "revision": "7f5a724a0c945753185c",
+    "url": "/Transcend/static/js/runtime~main.7f5a724a.js"
+  },
+  {
+    "revision": "df07e6fd4ce15de6718d",
+    "url": "/Transcend/static/js/main.df07e6fd.chunk.js"
+  },
+  {
+    "revision": "d156982f28d4b7636d43",
+    "url": "/Transcend/static/js/1.d156982f.chunk.js"
+  },
+  {
+    "revision": "df07e6fd4ce15de6718d",
+    "url": "/Transcend/static/css/main.6ca7e3a2.chunk.css"
+  },
+  {
+    "revision": "ed0945a73a95fd381af29e4394faff0e",
+    "url": "/Transcend/index.html"
+  }
+];
